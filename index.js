18cd05c1ecca9bf6d55273b9942c46fcdf8ef78f
Margaret <wanjirundirangu43@gmail.com>
2022-06-10 15:14:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 54;
+const num4 = 5;
+const multiply = num1 * num2;
 
-// Write your code here
+let random=getRandomValue();
+function getRandomValue(){
+  return Math.floor(Math.random()<1);
+}
+const mod = num3 % num4;
+let max= getMaxNumber();
+function getMaxNumber(){
+ return Math.max(20);
+}
+
